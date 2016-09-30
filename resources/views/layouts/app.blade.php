5b8f253ffb5c4eda8f1dfb59056531f631236afc
pensart <guy.pensart@hotmail.com>
2016-09-30 15:23:26 +0200

    added link to super user posts overview in the navbar home url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,24 +34,30 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/admin/posts') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
             </div>
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    &nbsp;
                 </ul>
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
+                    @if(auth()->user() !== null && auth()->user()->super)
+                        <li>
+                            {{--link to the super users post overview--}}
+                            <a href="{{ url('/superPosts') }}">Posts</a>
+                        </li>
+                    @endif
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
                     @else
                         <li class="dropdown">
+
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
